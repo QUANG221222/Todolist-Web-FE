d8f9a4775075fcda7aa1cc4d9de98d95bbd52507
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-09-06 14:19:35 +0700

    Refactor API_URL assignment for clarity in build mode handling

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,8 @@
 let apiRoot: string = ''
-if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://todolist-web-api.onrender.com'
-} else {
+if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
+} else {
+  apiRoot = 'https://todolist-web-api.onrender.com'
 }
+console.log('API Root:', apiRoot)
 export const API_URL = apiRoot
